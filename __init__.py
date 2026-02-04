bc15097963c9f62f56b96550c484385e21454e53
Luke_Zhang <baobinzhang1992@gmail.com>
2026-02-04 18:49:28 +0000

    📦 Move core files to root directory

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+LinguaHome - Language-Driven Smart Home Automation
+
+A code-generative LLM framework for conversational smart home control.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Baobin Zhang"
